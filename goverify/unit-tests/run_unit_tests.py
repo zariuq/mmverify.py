05107a567b93cd8471363e95200dbaa8e359dbc5
Zar Goertzel <zariuq@gmail.com>
2025-10-07 10:22:56 +0200

    Improve compressed proof handling and finalize tests

--- goverify/unit-tests/run_unit_tests.py
+++ goverify/unit-tests/run_unit_tests.py
@@ -422,7 +422,7 @@ th $p |- x $= ( ax ) A
   B
 \tC $.""",
         "should_reject": False,  # Should accept (whitespace ignored)
-        "error_keywords": [],
+        "error_keywords": ["warning"],
     },
 }
 
