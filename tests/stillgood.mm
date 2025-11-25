5f1da5d6ffba76efec72d12c21bfa9125e6a5541
Zar Goertzel <zariuq@gmail.com>
2025-11-25 01:30:51 +0100

    ...'

---
+++ tests/stillgood.mm
@@ -0,0 +1,18 @@
+$c formula |- ( ) $.
+
+$v x y z w $.
+xf $f formula x $.
+yf $f formula y $.
+zf $f formula z $.
+wf $f formula w $.
+combo $a formula ( x y ) $.
+${
+  $d x y $.
+  ax-1 $a |- ( x y ) $.
+$}
+
+${
+  $d x z $. $d x w $. $d y z $. $d y w $.
+  stillgood $p |- ( ( x y ) ( z w ) ) $=
+    xf yf combo zf wf combo ax-1 $.
+$}
